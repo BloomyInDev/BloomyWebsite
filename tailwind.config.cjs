7e77c6bdcd1b2f36cdc94d0cec331f2c6db02684
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2023-09-07 10:25:43 +0200

    New post ! :smiley:
    - New post
    - Switch from astro components to SVG pictures
    - Prettier lots of files
    - Modify a little the about page
    - Modify tailwind prose config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const disabledCss = {
+    'code::before': false,
+    'code::after': false,
+    pre: false,
+    code: false,
+    'pre code': false
+}
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('@tailwindcss/typography')],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: { css: disabledCss },
+                sm: { css: disabledCss },
+                lg: { css: disabledCss },
+                xl: { css: disabledCss },
+                '2xl': { css: disabledCss }
+            }
+        }
+    },
+    plugins: [require('@tailwindcss/typography')]
 }
