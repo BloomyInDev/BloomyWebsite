bcbb49a871876888e5eafdb71b6b637f705e07a4
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2024-03-17 21:52:13 +0100

    Update my website
    - Fix rss.xml not working since i've updated Astro
    - Added another description tag in the head

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
-export async function get() {
-  return rss({
-    title: 'Bloomy Blog',
-    description: 'Bloomy Blog',
-    site: 'https://bloomyindev.vercel.app',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
-  });
+export async function GET(context) {
+    return rss({
+        title: 'Bloomy Blog',
+        description: 'Bloomy Blog',
+        site: 'https://bloomyindev.me',
+        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        customData: `<language>en-us</language>`
+    })
 }
