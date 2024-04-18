e4268c0a2c8f7bd82d7a1895534babb8c22bd4aa
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2024-04-18 14:18:08 +0200

    Switch to Cloudflare Pages
    - Switch to Cloudflare Pages
    - Update every lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import prefetch from '@astrojs/prefetch'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import prefetch from '@astrojs/prefetch';
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), prefetch()]
-})
+  integrations: [tailwind(), prefetch()],
+  output: "server",
+  adapter: cloudflare()
+});
